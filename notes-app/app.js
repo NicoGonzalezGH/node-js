06f59f6a9f3c46e4757054b5ac881745d84b1f1e
Nico Gonzalez <nico.g-r@hotmail.com>
2021-08-05 23:28:19 -0300

    Segundo commit. Seguimos avanzando con el curso.

---
+++ notes-app/app.js
@@ -0,0 +1,7 @@
+const validator = require('validator');
+const getNotes = require('./notes.js');
+
+const message = getNotes();
+console.log(message);
+
+console.log(validator.isURL('http/mead.io'));
