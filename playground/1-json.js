d7c72a58f77cfe21f377845ec5022b6cf391f8fa
Nico Gonzalez <nico.g-r@hotmail.com>
2021-08-09 22:10:39 -0300

    Playground challenge completed

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,19 +1,13 @@
 const fs = require('fs');
-//const book = {
-//    title: 'Ego is the Enemy',
-//    author: 'Nico Gonzalez'
-//};
-//
-//Take and object and convert it to JSON
-//const bookJSON = JSON.stringify(book);
-//fs.writeFileSync('1-json.json', bookJSON);
-
-//const dataBuffer = fs.readFileSync('1-json.json');
-//const dataJSON = dataBuffer.toString();
-//const data = JSON.parse(dataJSON);
-//console.log(data.title);
 
 const dataBuffer = fs.readFileSync("1-json.json");
 const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data);
+const user = JSON.parse(dataJSON);
+console.log(user);
+
+user.name = 'Gonzalez';
+user.age = 29;
+
+
+const userJSON = JSON.stringify(user);
+fs.writeFileSync('1-json.json', userJSON);
